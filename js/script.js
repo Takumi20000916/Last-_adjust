c2cc29f41bd24b3a1e8da21f3c732c5c438d7f4b
Takumi20000916 <taku.s.0916@docomo.ne.jp>
2024-11-26 12:35:52 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,282 @@
+import { ObjectDetector, FilesetResolver } from "./vision_bundle.js";
+var objectDetector;
+let runningMode = "IMAGE";
+
+// モデルの切り替えを行う関数
+let modelSwitching = false;
+
+async function switchModel(modelPath) {
+    modelSwitching = true;  // モデル切り替え中フラグを設定
+    console.log("Switching model to:", modelPath);
+    try {
+        // モデルの準備
+        const vision = await FilesetResolver.forVisionTasks("./wasm");
+        // 新しい物体検出器を作成
+        const newObjectDetector = await ObjectDetector.createFromOptions(vision, {
+            baseOptions: {
+                modelAssetPath: modelPath,
+                delegate: "GPU"
+            },
+            scoreThreshold: 0.35,
+            runningMode: runningMode
+        });
+        // 古い物体検出器をnullに設定し、新しい物体検出器に置き換える
+        if (objectDetector) {
+            objectDetector = null;
+        }
+        objectDetector = newObjectDetector;
+        currentModel = modelPath;  // 現在のモデルを更新
+        console.log("Model switched successfully to:", modelPath);
+    } catch (error) {
+        console.error("Failed to switch model:", error);
+    } finally {
+        modelSwitching = false;  // モデル切り替え中フラグをリセット
+    }
+}
+
+// 初期化関数
+const initializeObjectDetector = async () => {
+    await switchModel('./models/hanyou.tflite');
+     // カメラを有効にする
+     enableCam();
+     // ローディングインジケーターを非表示にする
+     document.querySelector('#loading').style.display = 'none';
+};
+
+// ページロード時に初期化関数を呼び出す
+// window.addEventListener("load", () => {
+//     initializeObjectDetector();
+// });
+initializeObjectDetector();
+
+/********************************************************************
+// Demo 2: Continuously grab image from webcam stream and detect it.
+********************************************************************/
+let video = document.getElementById("webcam");
+let enableWebcamButton;
+
+function hasGetUserMedia() {
+    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+var children = [];
+
+async function enableCam(event) {
+    if (!objectDetector) {
+        console.log("Wait! objectDetector not loaded yet.");
+        return;
+    }
+
+    // localStorageに保存されたcameraIdがあれば、それを使用
+    const cameraId = localStorage.getItem('cameraId');
+
+    // カメラの制約を設定//ここでカメラのオプション
+    const constraints = {
+        video: {
+            deviceId: cameraId,
+            facingMode: 'environment',
+            width: { max: 1280 },
+            height: { max: 720 },
+            aspectRatio: { ideal: 1.0 },
+            frameRate: { max: 15 } // フレームレートを最大15fpsに制限
+        }
+    };
+
+
+      // ウェブカムストリームを有効にする
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            video.srcObject = stream;
+            window.currentStream = stream;
+
+            // ストリームの詳細情報を取得
+            let videoTrack = stream.getVideoTracks()[0];
+            let settings = videoTrack.getSettings();
+            let capabilities = videoTrack.getCapabilities();
+
+
+            video.addEventListener("loadeddata", predictWebcam);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+
+let detectionInterval = 800; // ミリ秒単位の検出間隔
+let lastDetectionTime = 0;
+
+async function predictWebcam() {
+    try {
+        const now = Date.now();
+        if (now - lastDetectionTime < detectionInterval) {
+            window.requestAnimationFrame(predictWebcam);
+            return;
+        }
+        lastDetectionTime = now;
+
+        if (runningMode === "IMAGE") {
+            runningMode = "VIDEO";
+            await objectDetector.setOptions({ runningMode: "VIDEO" });
+        }
+
+        const detections = await objectDetector.detectForVideo(video, now);
+        console.log("Detections:", detections); // デバッグ用
+
+        gotDetections(detections);
+        handleGestures();
+    } catch (error) {
+        console.error("Error in predictWebcam:", error);
+    }
+    window.requestAnimationFrame(predictWebcam);
+}
+
+
+
+
+
+
+
+// 信頼度のしきい値を変更するイベントリスナー
+document.querySelector('#input_confidence_threshold').addEventListener('change', changedConfidenceThreshold);
+
+// 信頼度のしきい値を変更する関数
+function changedConfidenceThreshold(e) {
+    let confidenceThreshold = parseFloat(e.srcElement.value);
+    objectDetector.setOptions({
+        // しきい値をfloatにキャスト
+        scoreThreshold: confidenceThreshold
+    });
+
+    document.querySelector('#confidence_threshold').innerHTML = e.srcElement.value;
+}
+
+// カメラのリストを取得する関数
+async function listCameras() {
+    try {
+        const selectCamera = document.getElementById('select_camera');
+        navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+                console.log(devices);
+                devices.forEach(device => {
+                    if (device.kind === 'videoinput') {
+                        const option = document.createElement('option');
+                        option.text = device.label || `camera ${selectCamera.length + 1}`;
+                        option.value = device.deviceId;
+                        
+                        // localStorageに保存されたcameraIdがあれば、それを選択状態にする
+                        const cameraId = localStorage.getItem('cameraId');
+                        if (cameraId === device.deviceId) {
+                            option.selected = true;
+                        }
+                        selectCamera.appendChild(option);
+                    }
+                });
+            });
+    } catch (err) {
+        console.error('メディアデバイスへのアクセス中にエラーが発生しました。', err);
+    }
+}
+await listCameras();
+
+// カメラのリフレッシュボタンを押した時のイベントリスナー
+document.querySelector('#button_refresh_camera').addEventListener('click', async () => {
+    try {
+        // 仮のカメラアクセスをリクエストしてユーザーの許可を取得
+        const initialStream = await navigator.mediaDevices.getUserMedia({ video: true });
+        document.querySelector('#select_camera').innerHTML = '';
+        await listCameras();
+
+        if (initialStream) {
+            initialStream.getTracks().forEach(track => track.stop());
+        }
+    } catch (err) {
+        console.error('メディアデバイスへのアクセス中にエラーが発生しました。', err);
+    }
+})
+
+// カメラ選択が変更された時のイベントリスナー
+document.getElementById('select_camera').addEventListener('change', changedCamera);
+function changedCamera() {
+    const selectCamera = document.getElementById('select_camera');
+    const constraints = {
+        video: {
+            deviceId: selectCamera.value,
+            facingMode: 'environment',
+            width: { max: 1920 },
+            height: { max: 1080 },
+            aspectRatio: { ideal: 1.0 }
+        }
+    };
+
+    // 選択されたカメラIDをlocalStorageに保存
+    localStorage.setItem('cameraId', selectCamera.value);
+
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function (stream) {
+            video.srcObject = stream;
+            video.addEventListener("loadeddata", predictWebcam);
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+let currentModel = './models/hanyou.tflite'; // 現在のモデル
+let gestureCounters = {}; // ジェスチャごとのカウントを記録するオブジェクト
+const gestureThreshold = 1; // 切り替えに必要な認識フレーム数
+
+function handleGestures() {
+    if (gestures_results) {
+        gestures_results.gestures.forEach((gestureData) => {
+            const gestureName = gestureData[0].categoryName;
+
+            // カウンタの初期化
+            if (!gestureCounters[gestureName]) {
+                gestureCounters[gestureName] = 0;
+            }
+
+            // 現在のジェスチャをカウントアップ
+            gestureCounters[gestureName]++;
+
+            // 他のジェスチャのカウンタをリセット
+            Object.keys(gestureCounters).forEach((key) => {
+                if (key !== gestureName) {
+                    gestureCounters[key] = 0;
+                }
+            });
+
+            // **カウントをコンソールに表示**
+            console.log(`Gesture: ${gestureName}, Count: ${gestureCounters[gestureName]}`);
+
+            // 120フレーム連続認識された場合、モデルを切り替え
+            if (gestureCounters[gestureName] >= gestureThreshold) {
+                switchModelByGesture(gestureName);
+                gestureCounters[gestureName] = 0; // カウンタをリセット
+            }
+        });
+    }
+}
+
+function switchModelByGesture(gestureName) {
+    if (gestureName === "Pointing_Up" && currentModel !== './models/hanyou.tflite') {
+        console.log(`Gesture: ${gestureName} detected. Switching model to hanyou.`);
+        currentModel = './models/hanyou.tflite';
+        switchModel(currentModel);
+    } else if (gestureName === "Victory" && currentModel !== './models/mickey.tflite') {
+        console.log(`Gesture: ${gestureName} detected. Switching model to mickey.`);
+        currentModel = './models/mickey.tflite';
+        switchModel(currentModel);
+    } else if (gestureName === "THREE" && currentModel !== './models/tempereture.tflite') {
+        console.log(`Gesture: ${gestureName} detected. Switching model to tempereture.`);
+        currentModel = './models/tempereture.tflite';
+        switchModel(currentModel);
+    } else if (gestureName === "FOUR" && currentModel !== './models/container3.tflite') {
+        console.log(`Gesture: ${gestureName} detected. Switching model to container3.`);
+        currentModel = './models/container3.tflite';
+        switchModel(currentModel);
+    }
+}
+
---
+++ js/sketch.js
@@ -0,0 +1,242 @@
+// バージョン情報
+let version = `
+last modified: 2023/06/01 01:34:31
+`;
+
+
+// 物体検出結果およびジェスチャー結果を格納する変数
+var object_results;
+let gestures_results;
+
+
+// P5.jsのセットアップ
+function setup() {
+    // P5.jsのキャンバスを作成し、HTMLの要素に関連付ける
+    let p5canvas = createCanvas(400, 400);
+    p5canvas.parent('#canvas');
+
+    document.querySelector('#version').innerHTML = version;
+
+
+    // ジェスチャー結果を取得する関数
+    gotGestures = function (results) {
+        gestures_results = results;
+        adjustCanvas();
+    }
+
+
+    // ものが見つかると以下の関数が呼び出される．resultsに検出結果が入っている．
+    gotDetections = function (_results) {
+        object_results = _results;
+        strokeWeight(5)
+        let video_width = document.querySelector('#webcam').videoWidth;
+        let video_height = document.querySelector('#webcam').videoHeight;// object_results.detections内の各物体検出に対するループ
+
+        // object_results.detections内の各物体検出に対するループ
+        for (let d of object_results.detections) {
+            let bb = d.boundingBox;// バウンディングボックス（bb）は、現在の物体検出に関連する情報を含むオブジェクト
+            let ratio = {// ratioオブジェクトは、キャンバスサイズとウェブカメラ映像サイズとの比率を計算
+                x: width / video_width,
+                y: height / video_height
+            };
+            bb.originX = ratio.x * bb.originX;// バウンディングボックスのX座標をキャンバスサイズに合わせて調整
+            bb.originY = ratio.y * bb.originY;// バウンディングボックスのY座標をキャンバスサイズに合わせて調整
+            bb.width *= ratio.x;// バウンディングボックスの幅をキャンバスサイズに合わせて調整
+            bb.height *= ratio.y;// バウンディングボックスの高さをキャンバスサイズに合わせて調整
+        }
+        adjustCanvas();
+    }
+    document.querySelector('#version').innerHTML = version;// バージョン情報を表示
+
+    gotGestures = function (results) {
+        gestures_results = results;
+        handleGestures(); // ジェスチャー結果の処理を呼び出す
+    }
+}
+
+
+
+
+const synth = window.speechSynthesis;
+const utterance = new SpeechSynthesisUtterance();
+let isSpeaking = false; // 読み上げ中かどうかを示すフラグ
+
+function speakText() {
+    synth.speak(utterance);
+    isSpeaking = true; // 読み上げ中フラグを設定
+
+    utterance.onend = function (event) {
+        // 読み上げが完了したときの処理
+        isSpeaking = false; // 読み上げが完了したらフラグをリセット
+    };
+}
+let count = 0;
+//pointing_upの時に<button id="speakButton">音声読み上げ</button>ボタンをクリックしたことにして、テキストを読み上げる
+function handleGestures() {
+    if (gestures_results) {
+        for (let i = 0; i < gestures_results.gestures.length; i++) {
+            let name = gestures_results.gestures[i][0].categoryName;  // ジェスチャーのカテゴリ名 
+            if (name === "Pointing_Up" || name === "Victory" || name === "THREE" || name === "FOUR") {
+                noFill();
+                noStroke();
+                rect(0, 0, 640, 480);
+                stroke(250);
+                document.querySelector('#speakButton').click();
+                
+            }
+        }
+    }
+}
+//ボタンが押されたら読み上げるが、1回読み上げたらもう読み上げない
+
+document.querySelector('#speakButton').addEventListener('click', () => {
+    
+    if(!count == 1){
+        utterance.text = "音声読み上げ開始";
+        speakText();
+        count = 1;
+    }
+    for (let detection of object_results.detections) {
+        let index = detection.categories[0].index;// カテゴリのインデックスを取得
+        let bb = detection.boundingBox;// バウンディングボックス（物体の境界ボックス）情報を取得
+        let name = detection.categories[0].categoryName;// 物体のカテゴリ名を取得
+        let score = detection.categories[0].score;// 物体検出の信頼度を取得
+        let c = [240, 230, 140];// カテゴリに対応する色を取得
+        c = [...c, 250];// 色に透明度（Alpha）の値を追加して、半透明に設定
+        stroke(c);//矩形（personとか書いてあるとこ）の枠線の色を設定
+        strokeWeight(2);
+        noFill();
+        //人差し指より完全に上にある物体で一番近い物体のバウンディングボックスのみを表示
+        if (bb.originX < Pointing_x && bb.originX + bb.width > Pointing_x && bb.originY + bb.height < Pointing_y) {
+            rect(bb.originX, bb.originY, bb.width, bb.height);// バウンディングボックスを描画
+
+            fill(c);// 矩形の塗りつぶし色を設定
+            // バウンディングボックスの下に別の矩形を描画
+            rect(bb.originX, bb.originY - 20, bb.width, 20);
+            
+            //もし名前がpersonだったら、人、robotだったらロボット、banabaだったらバナナと表示
+            if (name === "14-green_tea") {
+                name = "緑茶";
+            } else if (name === "17-jasmine_tea") {
+                name = "ジャスミン茶";
+            } else if (name === "18-barley_tea") {
+                name = "麦茶";
+            }else if (name === "29-cider") {
+                name = "麦茶";
+            } else if (name === "35-peach") {
+                name = "ピーチ";
+            }else if (name === "25-unsweetened_coffee") {
+                name = "ブラックコーヒー";
+            } else if (name === "13-water") {
+                name = "水";
+            }else if (name === "31-sports") {
+                name = "スポーツドリンク";
+            }
+
+            else if (name === "4-cold") {
+                name = "冷たい";
+            } else if (name === "5-hot") {
+                name = "あたたかい";
+            } 
+
+            //ドリンク種類のカテゴリ名を日本語に変換
+            else if (name === "6-tea") {
+                name = "お茶";
+            }else if (name === "7-black_tea") {
+                name = "紅茶";
+            } else if (name === "8-coffee") {
+                name = "コーヒー";
+            }else if (name === "9-juice") {
+                name = "ジュース";
+            } else if (name === "10-carbonated") {
+                name = "炭酸";
+            }else if (name === "11-dessert") {
+                name = "デザート";
+            }
+            else if (name === "12-food") {
+                name = "食べ物";
+            }
+
+            //容器情報のカテゴリ名を日本語に変換
+            else if (name === "1-pet") {
+                name = "ペットボトル";
+            } else if (name === "2-can") {
+                name = "缶";
+            }else if (name === "3-bottle") {
+                name = "瓶";
+            }
+            
+            //情報の表示
+            noStroke();
+            fill(255);
+            textSize(20);
+            textAlign(LEFT, CENTER);
+            text(`${name} - ${score.toFixed(2)} `, bb.originX + 10, bb.originY - 10);// カテゴリ名と信頼度を描画
+            if(!isSpeaking){
+                utterance.text = name + "です";
+                speakText();
+            }
+            index++;
+        }
+        
+    }
+});
+
+let Pointing_x;
+let Pointing_y;
+function draw() {
+
+    clear();
+    // console.log(count);
+    if (gestures_results) {      
+        // もしジェスチャーの結果が存在する場合、以下の処理を実行
+        if (gestures_results.landmarks) {
+
+                   
+            for (const landmarks of gestures_results.landmarks) {
+                noStroke();  // 線を描かない
+                fill(100, 150, 210);  // 円の塗りつぶし色を指定
+                Pointing_x = gestures_results.landmarks[0][8].x * width;
+                Pointing_y = gestures_results.landmarks[0][8].y * height;
+                circle(Pointing_x, Pointing_y, 10);  // 特徴点の位置に円を描画
+            }
+        }
+    
+        // ジェスチャーの結果を表示する Pointing_Upは3番目
+        for (let i = 0; i < gestures_results.gestures.length; i++) {
+            // 各検出されたジェスチャーの情報を処理
+            noStroke();  // 線を描かない
+            fill(255, 0, 0);  // 文字の色を指定
+            textSize(20);  // テキストのサイズを指定
+    
+            // ジェスチャーの情報を変数に格納
+            let name = gestures_results.gestures[i][0].categoryName;  // ジェスチャーのカテゴリ名
+
+            textSize(30);  // 大きなテキストサイズを指定
+            fill(0);  // 文字の色を指定
+            text(name, 100,100);
+        }   
+        handleGestures();
+        //console.log("うんこぶりぶり");
+    }
+}
+
+
+// キャンバスのサイズを調整する関数
+function adjustCanvas() {
+    // ウェブカメラの映像サイズに合わせてキャンバスのサイズを調整
+    var element_webcam = document.getElementById('webcam');
+    resizeCanvas(element_webcam.clientWidth, element_webcam.clientHeight);
+    //console.log(element_webcam.clientWidth);
+}
+
+// カメラの再生/停止を切り替える関数
+function toggleCameraPlay() {
+    let element_video = document.querySelector('#webcam');
+    if (element_video.paused) {
+        element_video.play();
+    } else {
+        element_video.pause();
+    }
+}
+
